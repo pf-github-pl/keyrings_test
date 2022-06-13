50c8d7b7777b882d63125abde6784d3a6633451c
pf-github-pl <pf.github@gmail.com>
2022-06-13 16:43:45 +0200

    convert validator function to decorator

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import keyring
 import getpass
 
 
-def print_secret(validated:bool):
-	if validated:
-		return print('*'*52, '* This is my secret message for logged in users ;) *', '*'*52, sep='\n')
-
-
 def set_new_password():
 	password = getpass.getpass("New password: ")
 	password_rep = getpass.getpass("Repeat new password: ")
@@ -18,23 +13,32 @@ def set_new_password():
 
 
 def validate(username):
-	if keyring.get_password('system', username) == None:
-		print("User does not exists! Please provide password to create new account. ")
-		set_new_password()
+	def wrapper(function):
+		def inner_wrapper(*args, **kwargs):
 
-	elif keyring.get_password('system', username) != None:
-		password = getpass.getpass("Password: ")
+			if keyring.get_password('system', username) == None:
+				print("User does not exists! Please provide password to create new account. ")
+				set_new_password()
+				
+			elif keyring.get_password('system', username) != None:
+				password = getpass.getpass("Password: ")
 
-		if password != keyring.get_password('system', username):
-			print("Wrong password. Access Denied!")
+				if password != keyring.get_password('system', username):
+					print("Wrong password. Access Denied!")
 
-		elif password == keyring.get_password('system', username):
-			print(f"You are logged in now. It's nice to see you {username}!")
-			return True
+				elif password == keyring.get_password('system', username):
+					print(f"You are logged in now. It's nice to see you {username}!")
+					return function(*args, **kwargs)
+
+		return inner_wrapper
+
+	return wrapper
 
 
 username = input("Username: ")
 
-is_validated = validate(username)
+@validate(username)
+def print_secret():
+	return print('*'*52, '* This is my secret message for logged in users ;) *', '*'*52, sep='\n')
 
-print_secret(is_validated)
+print_secret()
